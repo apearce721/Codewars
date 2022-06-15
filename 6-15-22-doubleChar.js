a85fcb0b7db5071e41011c2eab4216759d0c65d4
Alyssa Pearce <101526036+apearce721@users.noreply.github.com>
2022-06-15 19:31:56 -0400

    Add files via upload
    6-15-22 Codewars challenge, 8kyu

---
+++ 6-15-22-doubleChar.js
@@ -0,0 +1,9 @@
+/* Given a string, you have to return a string in which each character (case-sensitive) is 
+repeated once. */
+
+
+function doubleChar(str) {
+    return str.split('').map(n => n+n).join('')
+  }
+  
+
