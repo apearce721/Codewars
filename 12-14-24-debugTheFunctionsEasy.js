bc38d6ff75eb4db7806f864b54b95055e773f187
Alyssa Pearce <101526036+apearce721@users.noreply.github.com>
2024-12-14 14:21:51 -0500

    Add files via upload

---
+++ 12-14-24-debugTheFunctionsEasy.js
@@ -0,0 +1,14 @@
+/* Debug the functions
+Should be easy, begin by looking at the code. Debug the code and the functions should work.
+
+There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR) */
+
+function multi(arr) {
+    return arr.reduce((acc, c) => acc *= c)
+  }
+  function add(arr) {
+    return arr.reduce((acc, c) => acc += c);
+  }
+  function reverse(str) {
+    return str.split('').reverse().join('');
+  }
