b6dbf81e1315c1c8ff6e06fb1313194e667adffd
Alyssa Pearce <101526036+apearce721@users.noreply.github.com>
2025-01-19 16:31:47 -0500

    Add files via upload

---
+++ 1-19-25-exclamationMarksSeries6RemoveNExclamationMarks.js
@@ -0,0 +1,19 @@
+/* Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi" */
+
+function remove(s, n) {
+    let count = 0;
+    return s.split('').filter(char => char !== '!' || ++count > n).join('');
+  }
