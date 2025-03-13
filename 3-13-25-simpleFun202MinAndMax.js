1ade09a27e511c9df4c7a7405babe2d6c2c5968c
Alyssa Pearce <101526036+apearce721@users.noreply.github.com>
2025-03-13 14:20:15 -0400

    Add files via upload

---
+++ 3-13-25-simpleFun202MinAndMax.js
@@ -0,0 +1,46 @@
+/* Task
+You are given three integers l, d and x. Your task is:
+
+• determine the minimal integer n 
+    such that l ≤ n ≤ d, and the sum of its digits equals x.
+• determine the maximal integer m 
+    such that l ≤ m ≤ d, and the sum of its digits equals x.
+It is guaranteed that such numbers always exist.
+
+Input/Output
+[input] integer l
+
+[input] integer d
+
+1 ≤ l ≤ d ≤ 10000.
+
+[input] integer x
+1 ≤ x ≤ 36
+
+[output] an integer array
+Array of two elements, where the first element is n, and the second one is m.
+
+Example
+For l = 500, d = 505, x = 10, the output should be [505, 505].
+
+For l = 100, d = 200, x = 10, the output should be [109, 190]. */
+
+function minAndMax(l, d, x) {
+    //find minimal integer n
+    let minimalInt = null
+    for(let i = l; i <= d; i++){
+      if(i.toString().split('').map(Number).reduce((acc,c) => acc + c) === x){
+        minimalInt = i
+        break
+      }
+    }
+      //find maximal integer m
+      let maximalInt = null
+      for(let i = d; i >= l; i--){
+        if(i.toString().split('').map(Number).reduce((acc,c) => acc + c) === x){
+          maximalInt = i
+          break
+      }
+    }  
+    return [minimalInt, maximalInt]
+  }
