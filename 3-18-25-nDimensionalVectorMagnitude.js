9eab19580dec856a39bbd31e2f9da8ffe9506c07
Alyssa Pearce <101526036+apearce721@users.noreply.github.com>
2025-03-18 09:51:14 -0400

    Add files via upload

---
+++ 3-18-25-nDimensionalVectorMagnitude.js
@@ -0,0 +1,10 @@
+/* Return the magnitude of a vector, given as an array of its component values.
+
+The vector (2, 3, 5) would be represented as an array containing three values at indices 0, 1 and 2 respectively.
+The array will always contain at least 2 and at most 100 elements (2 ≤ vector.Length ≤ 100), each of which will be within the range [-100, 100].
+An array containing four elements represents a vector in 4D space. */
+
+
+function magnitude(vector) {
+    return Math.sqrt(vector.reduce((acc,c) => acc + c**2, 0))
+}
